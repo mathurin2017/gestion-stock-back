f93f9349ade9588a928f7d0ea70631594b6020bf
Mathurin BALACOUNE <mathurin.balacoune@gmail.com>
2023-08-05 00:13:37 +0200

    First commit

---
+++ src/main/java/com/lloufa/gestionstockback/GestionStockBackApplication.java
@@ -0,0 +1,13 @@
+package com.lloufa.gestionstockback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lloufa/gestionstockback/GestionStockBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lloufa.gestionstockback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStockBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
